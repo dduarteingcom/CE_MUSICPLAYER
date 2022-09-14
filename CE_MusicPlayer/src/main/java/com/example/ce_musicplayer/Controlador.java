53c16a27ccd3310a0195ee1857239b6a409b8249
MauroCR52 <mauluna52@gmail.com>
2022-09-14 13:07:03 -0600

    Ventana de inicio de sesion con sus entradas y lista de provincias

--- CE_MusicPlayer/src/main/java/com/example/ce_musicplayer/Controlador.java
+++ CE_MusicPlayer/src/main/java/com/example/ce_musicplayer/Controlador.java
@@ -1,14 +1,49 @@
 package com.example.ce_musicplayer;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class Controlador {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controlador implements Initializable{
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button boton_iniciar;
+
+    @FXML
+    private TextField correo_entry;
+
+    @FXML
+    private Label correo_label;
+
+    @FXML
+    private TextField nom_entry;
+
+    @FXML
+    private Label nom_label;
+
+    @FXML
+    private TextField pass_entry;
+
+    @FXML
+    private Label pass_label;
+
+    @FXML
+    private Label prov_label;
+
+    @FXML
+    private ChoiceBox<String> prov_lista;
+    private String[] provincias = {"San Jose","Cartago","Alajuela","Puntarenas",
+    "Heredia","Guanacaste"};
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        prov_lista.getItems().addAll(provincias);
+
     }
 }
