4b542e22bb59963455c6342259efae47ce8a48a4
MauroCR52 <mauluna52@gmail.com>
2022-09-14 14:15:42 -0600

    Función de botón para pasar de ventana

--- CE_MusicPlayer/src/main/java/com/example/ce_musicplayer/Main.java
+++ CE_MusicPlayer/src/main/java/com/example/ce_musicplayer/Main.java
@@ -2,6 +2,7 @@ package com.example.ce_musicplayer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,9 +11,8 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Ventana_sesion.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Ventana_sesion.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
