748493ad8b361973b024eae2447991c429a3bb4e
MauroCR52 <mauluna52@gmail.com>
2022-09-17 12:39:46 -0600

    Corrección de errores

--- CE_MusicPlayer/src/main/java/com/example/ce_musicplayer/Controlador_biblio1.java
+++ CE_MusicPlayer/src/main/java/com/example/ce_musicplayer/Controlador_biblio1.java
@@ -2,21 +2,19 @@ package com.example.ce_musicplayer;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.stage.Stage;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class Controlador_biblio1 implements Initializable {
+public class Controlador_biblio1 {
+
+    @FXML
+    private ProgressBar barraCancion;
 
     @FXML
     private MenuBar barra_menu;
@@ -24,14 +22,54 @@ public class Controlador_biblio1 implements Initializable {
     @FXML
     private MenuItem cerrar_sesion;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    @FXML
+    private Button modoButton;
+
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    private Button pauseButton;
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private Button prevButton;
+
+    @FXML
+    private Label songLabel;
+
+    @FXML
+    private Slider volumen;
+
+    @FXML
+    void cerrarSesion(ActionEvent event) throws IOException {
+        Main m = new Main();
+        m.cambioEscena("Ventana_sesion.fxml");
+    }
+    @FXML
+    void modoContinuo(ActionEvent event) {
 
     }
 
-    public void cerrarSesion(ActionEvent event) throws IOException {
-        Main m = new Main();
-        m.cambioEscena("Ventana_sesion.fxml");
+    @FXML
+    void nextCancion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pausar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void prevCancion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reproducir(ActionEvent event) {
 
     }
 
