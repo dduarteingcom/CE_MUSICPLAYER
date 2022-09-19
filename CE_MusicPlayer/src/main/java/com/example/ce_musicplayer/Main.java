0af28ef475e3569736809b303b0bea6219905dcd
MauroCR52 <mauluna52@gmail.com>
2022-09-19 13:52:02 -0600

    Bromita 2.0

--- CE_MusicPlayer/src/main/java/com/example/ce_musicplayer/Main.java
+++ CE_MusicPlayer/src/main/java/com/example/ce_musicplayer/Main.java
@@ -5,23 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import jssc.SerialPort;
-import jssc.SerialPortEvent;
-import jssc.SerialPortException;
 
 import java.io.IOException;
 
 
 public class Main extends Application {
     private static Stage stg;
-    static SerialPort serial_Port;
 
     @Override
     public void start(Stage stage) throws Exception {
 
 
         stg = stage;
-        Parent root = FXMLLoader.load(getClass().getResource("Ventana_biblioteca1.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Ventana_sesion.fxml"));
         stage.setScene(new Scene(root));
         stage.show();
 
@@ -33,19 +29,6 @@ public class Main extends Application {
         b1.InsertarCan(cancion2);
         b1.InsertarCan(cancion3);
 
-        //b1.GuardarBiblio();
-        //b1.eliminarBiblio();
-        //Biblioteca biblioteca2 = new Biblioteca("prueba");
-        //biblioteca2.prueba(3);
-        //b1.EditarNombreBiblio("Punk");
-        Bibliotecas BMauricio=new Bibliotecas();
-        Bibliotecas BDaniel=new Bibliotecas();
-        Bibliotecas BMbappe=new Bibliotecas();
-
-
-       // BMauricio.LectorBM();
-
-
 
     }
 
@@ -55,36 +38,17 @@ public class Main extends Application {
     }
 
 
-    public static void arduino(){
-        SerialPort port = new SerialPort("COM3");
-        try {
-            port.openPort();
-            port.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-
-            port.addEventListener((SerialPortEvent event)->{
-                if(event.isRXCHAR()){
-                    try {
-                        if (port.readString().equals("1")){
-                            System.out.println("Hola");
-                        }
-                    } catch (SerialPortException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            });
-
-        } catch (SerialPortException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
 
     public static void main(String[] args) throws IOException {
 
 
-
         Lista_usuarios.lista_usuarios.insertarUsuario(new Usuario("Mauricio", "mauluna52@gmail.com", "Cartago", "Valeria26"));
         Lista_usuarios.lista_usuarios.insertarUsuario(new Usuario("Daniel", "dduarte@gmail.com", "San Jose", "Dduarte55"));
+        Lista_usuarios.lista_usuarios.insertarUsuario(new Usuario("Mbappe", "mfrappe@gmail.com", "Alajuela", "Mfrappe88"));
+
         launch();
+
+
     }
 }
